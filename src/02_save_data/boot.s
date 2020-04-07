70b7700ffc4d78b8a0721882e9f8960bcb08f514
tomiharu0317 <tomiharu1017@gmail.com>
2020-04-07 17:02:43 +0900

    add 02_save_data directory and change boot.s file.

---
+++ src/02_save_data/boot.s
@@ -0,0 +1,41 @@
+        BOOT_LOAD       equ     0x7c00
+
+        ORG     BOOT_LOAD
+
+;エントリポイント
+
+entry:
+        jmp     ipl
+
+        ; BIOS Parameter Block
+
+        times 90 - ($ - $$) db 0x90
+
+        ; Initial Program Loader
+
+ipl:
+
+        cli                                 ;割り込みの禁止
+
+        mov     ax, 0x0000
+        mov     ds, ax
+        mov     es, ax
+        mov     ss, ax
+        mov     sp, BOOT_LOAD
+
+        sti                                 ;割り込みの許可
+
+        mov     [BOOT.DRIVE], dl            ;ブートドライブを保存
+
+        ; 処理の終了
+
+        jmp     $
+
+ALIGN 2, db 0
+BOOT:                                       ;ブートドライブに関する情報
+.DRIVE:         dw  0                       ;ドライブ番号
+
+;ブートフラグ(512biteの終了)
+
+        times   510 - ($ - $$) db 0x00
+        db 0x55, 0xAA
