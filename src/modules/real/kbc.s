d8046a6e2a3b14864b7406b11c1c1d14d1202e16
tomiharu0317 <tomiharu1017@gmail.com>
2020-04-11 13:40:02 +0900

    implemented kbc cmd write func

--- src/modules/real/kbc.s
+++ src/modules/real/kbc.s
@@ -72,3 +72,39 @@ KBC_Data_Read:
             pop     bp
 
             ret
+
+KBC_Cmd_Write:
+
+            ;construct stack frame
+                                                        ; BP +4 | data
+            push    bp
+            mov     bp, sp
+
+            ;save register
+            push    cx
+
+            ;process
+            mov     cx, 0                               ; CX = 0; // max count value
+.10L:
+            in      al, 0x64
+            test    al, 0x02                            ; ZF = AL & 0x02 // B1:input buffer full
+            loopnz  .10L
+
+            cmp     cx, 0
+            jz      .20E                                ; timeout
+
+            mov     al, [bp + 4]
+            out     0x64, al
+.20E:
+
+            mov     ax, cx                              ; return CX
+
+            ;return register
+            pop     cx
+
+            ;destruct stack frame
+
+            mov     sp, bp
+            pop     bp
+
+            ret
