8549b416cbc7b61e4f9f13d3d1669935e06e9014
tomiharu0317 <tomiharu1017@gmail.com>
2020-04-11 13:32:13 +0900

    implemented kbc data read func

--- src/modules/real/kbc.s
+++ src/modules/real/kbc.s
@@ -5,7 +5,7 @@ KBC_Data_Write:
             push    bp
             mov     bp, sp
 
-            ;save registers
+            ;save register
             push    cx
 
             ;process
@@ -24,7 +24,7 @@ KBC_Data_Write:
 
             mov     ax, cx                              ; return CX
 
-            ;return registers
+            ;return register
             pop     cx
 
             ;destruct stack frame
@@ -34,3 +34,41 @@ KBC_Data_Write:
 
             ret
 
+KBC_Data_Read:
+
+            ;construct stack frame
+
+            push    bp
+            mov     bp, sp
+
+            ;save register
+            push    cx
+
+            ;process
+            mov     cx, 0
+.10L:
+            in      al, 0x64
+            test    al, 0x01                            ; ZF = AL & 0x01 // B0:output buffer full
+            loopnz  .10L
+
+            cmp     cx, 0
+            jz      .20E
+
+            mov     ah, 0x00
+            in      al, 0x60
+
+            mov     di, [bp + 4]                        ; DI = Address where read data will be saved
+            mov     [di + 0], ax
+.20E:
+
+            mov     ax, cx
+
+            ;return register
+            pop     cx
+
+            ;destruct stack frame
+
+            mov     sp, bp
+            pop     bp
+
+            ret
