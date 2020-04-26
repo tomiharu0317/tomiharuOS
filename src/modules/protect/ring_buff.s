39d9430007ae611daba525c4efe0de01d37f32f8
tomiharu0317 <tomiharu1017@gmail.com>
2020-04-27 00:23:11 +0900

    implemented new func writing data to ring buffer

--- src/modules/protect/ring_buff.s
+++ src/modules/protect/ring_buff.s
@@ -26,7 +26,7 @@ ring_rd:
 
             inc     ebx                                                 ;   EBX++           // next reading location
             and     ebx, RING_INDEX_MASK                                ;   EBX &= 0x0F     // limit size
-            mov     [esi + ring_buff.rp], ebx                           ;   wp = EBX        // save the reading location
+            mov     [esi + ring_buff.rp], ebx                           ;   rp = EBX        // save the reading location
 
             mov     eax, 1                                              ;   EAX = 1         // data exists
 .10E:                                                                   ; }
@@ -34,6 +34,52 @@ ring_rd:
             ; return registers
             pop     edi
             pop     esi
+            pop     ebx
+            pop     eax
+
+            ; destruct stack frame
+            mov     esp, ebp
+            pop     ebp
+
+            ret
+
+ring_wr:
+
+            ; construct stack frame                                     ;    +12 | data to write
+            push    ebp                                                 ; EBP+ 8 | ring buffer
+            mov     ebp, esp
+
+            ; save registers
+            push    eax
+            push    ebx
+            push    ecx
+            push    esi
+            push    edi
+
+            ; get args
+            mov     esi, [ebp + 8]
+
+            ; confirm the writing location
+            mov     eax, 0
+            mov     ebx, [esi + ring_buff.wp]                           ; writing position
+            mov     ecx, ebx
+            inc     ecx                                                 ; next writing location
+            and     ecx, RING_INDEX_MASK                                ; size limit
+
+            cmp     ecx, [esi + ring_buff.rp]                           ; if (ECX != rp)
+            je      .10E                                                ; {
+
+            mov     al, [ebp + 12]                                      ;   AL = data
+
+            mov     [esi + ring_buff.item + ebx], al                    ;   BUFFER[wp] = AL // save key code
+            mov     [esi + ring_buff.wp], ecx                           ;   wp = ECX        // save the writing location
+            mov     eax, 1
+.10E:
+
+            ; return registers
+            pop     edi
+            pop     esi
+            pop     ecx
             pop     ebx
             pop     eax
 
