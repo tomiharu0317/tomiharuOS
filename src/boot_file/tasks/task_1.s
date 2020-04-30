92b57b806485ddade464e9b32350403fa31a5b0e
tomiharu0317 <tomiharu1017@gmail.com>
2020-04-30 13:38:37 +0900

    implemented non-preemptive multitask

--- src/boot_file/tasks/task_1.s
+++ src/boot_file/tasks/task_1.s
@@ -3,8 +3,24 @@ task_1:
             ; display string
             cdecl   draw_str, 63, 0, 0x07, .s0
 
-            ; end of task
-            iret
+;-------------------------------------------------------------
+; non-preemptive multitask
+;-------------------------------------------------------------
+
+.10L:
+            ; display time
+            mov     eax, [RTC_TIME]                         ; get time
+            cdecl   draw_time, 72, 0, 0x0700, eax
+
+            ; call Task
+            jmp    SS_TASK_0:0                             ; jump to Task0(kernel)
+
+            jmp     .10L
+
+;-------------------------------------------------------------
+
+            ; ; end of task
+            ; iret
 
             ; data
 .s0:        db  "Task_1", 0
