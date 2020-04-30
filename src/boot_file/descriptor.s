fb7010dc6595af63979e6397d19dbcfc46d6caad
tomiharu0317 <tomiharu1017@gmail.com>
2020-04-30 16:48:09 +0900

    define call gate descriptor

--- src/boot_file/descriptor.s
+++ src/boot_file/descriptor.s
@@ -68,6 +68,7 @@ GDT:            dq  0x0000000000000000
 .ldt            dq  0x0000820000000000                          ; LDT descriptor
 .tss_0:         dq  0x0000890000000067                          ; TSS descriptor
 .tss_1:         dq  0x0000890000000067                          ; TSS descriptor
+.call_gate:     dq  0x0000EC0400080000                          ; 386 call gate(DPL=3, count=4, SEL=8)
 .end:
 
 CS_KERNEL       equ .cs_kernel - GDT
@@ -75,6 +76,7 @@ DS_KERNEL       equ .ds_kernel - GDT
 SS_LDT          equ .ldt       - GDT
 SS_TASK_0       equ .tss_0     - GDT
 SS_TASK_1       equ .tss_1     - GDT
+SS_GATE_0       equ .call_gate - GDT
 
 GDTR:   dw      GDT.end - GDT - 1
         dd      GDT
@@ -86,13 +88,13 @@ GDTR:   dw      GDT.end - GDT - 1
 LDT:            dq  0x0000000000000000                          ; NULL
 .cs_task_0:     dq  0x00CF9A000000FFFF                          ; CODE 4G
 .ds_task_0:     dq  0x00CF92000000FFFF                          ; DATA 4G
-.cs_task_1:     dq  0x00CF9A000000FFFF                          ; CODE 4G
-.ds_task_1:     dq  0x00CF92000000FFFF                          ; DATA 4G
+.cs_task_1:     dq  0x00CFFA000000FFFF                          ; CODE 4G
+.ds_task_1:     dq  0x00CFF2000000FFFF                          ; DATA 4G
 .end:
 
 CS_TASK_0       equ (.cs_task_0 - LDT) | 4                      ; cs selector for task0 // set bit 2(TI)
-DS_TASK_0       equ (.ds_task_0 - LDT) | 4                      ; ds selector for task0
-CS_TASK_1       equ (.cs_task_1 - LDT) | 4                      ; cs selector for task1
-DS_TASK_1       equ (.ds_task_1 - LDT) | 4                      ; ds selector for task1
+DS_TASK_0       equ (.ds_task_0 - LDT) | 4                      ; ds selector for task0 // Priviledge level 0
+CS_TASK_1       equ (.cs_task_1 - LDT) | 4 | 3                  ; cs selector for task1 // Priviledge level 3
+DS_TASK_1       equ (.ds_task_1 - LDT) | 4 | 3                  ; ds selector for task1
 
 LDT_LIMIT       equ .end        - LDT - 1
