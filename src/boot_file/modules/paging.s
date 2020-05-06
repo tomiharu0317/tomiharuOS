b549918bfe860be8687e61af03246a463d5bfb61
tomiharu0317 <tomiharu1017@gmail.com>
2020-05-06 12:30:12 +0900

    exchange init_page, page_set_4m and 0x00000007

--- src/boot_file/modules/paging.s
+++ src/boot_file/modules/paging.s
@@ -1,43 +1,3 @@
-page_set_4m:
-
-            ; construct stack frame
-            push    ebp
-            mov     ebp, esp                                        ; EBP+8 | base address of page directory
-
-            ; save registers
-            pusha
-
-            ; make page directory
-            cld
-            mov     edi, [ebp + 8]
-            mov     eax, 0x00000000                                 ; // P = 0
-            mov     ecx, 1024
-            rep     stosd                                           ; while(ecx--) *edi++ = type
-
-            ; set top entry
-            mov     eax, edi                                        ; EAX = right after the page directory = address of page table
-            and     eax, ~0x0000_0FFF                               ; specifying the physical address
-            or      eax, 7                                          ; permit R/W
-            mov     [edi - (1024 * 4)], eax                         ; set top entry of page directory
-
-            ; set page table
-            mov     eax, 0x000000007                                ; specifying physical address and permit R/W
-            mov     ecx, 1024
-
-.10L:
-            stosd
-            add     eax, 0x00001000
-            loop    .10L
-
-            ; return registers
-            popa
-
-            ; desctruct stack frame
-            mov     esp, ebp
-            pop     ebp
-
-            ret
-
 init_page:
 
             ; save registers
@@ -66,5 +26,45 @@ init_page:
 
             ; return registers
             popa
+
+            ret
+
+page_set_4m:
+
+            ; construct stack frame
+            push    ebp
+            mov     ebp, esp                                        ; EBP+8 | base address of page directory
+
+            ; save registers
+            pusha
+
+            ; make page directory
+            cld
+            mov     edi, [ebp + 8]
+            mov     eax, 0x00000000                                 ; // P = 0
+            mov     ecx, 1024
+            rep     stosd                                           ; while(ecx--) *edi++ = type
+
+            ; set top entry
+            mov     eax, edi                                        ; EAX = right after the page directory = address of page table
+            and     eax, ~0x0000_0FFF                               ; specifying the physical address
+            or      eax, 7                                          ; permit R/W
+            mov     [edi - (1024 * 4)], eax                         ; set top entry of page directory
+
+            ; set page table
+            mov     eax, 0x00000007                                ; specifying physical address and permit R/W
+            mov     ecx, 1024
+
+.10L:
+            stosd
+            add     eax, 0x00001000
+            loop    .10L
+
+            ; return registers
+            popa
+
+            ; desctruct stack frame
+            mov     esp, ebp
+            pop     ebp
 
             ret
